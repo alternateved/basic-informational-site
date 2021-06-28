9186fdc07666a7f1779d83c408987ba21843b53d
alternateved <alternateved@gmail.com>
2021-06-28 22:09:48 +0200

    Rework index.js and add a little styling

--- index.js
+++ index.js
@@ -2,38 +2,41 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-http
-  .createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const filename = '.' + q.pathname;
+const server = http.createServer((req, res) => {
+  const q = url.parse(req.url, true);
+  const filename = '.' + q.pathname;
 
-    if (filename == './') {
-      fs.readFile('./index.html', (err, data) => {
-        if (err) {
-          console.error(err);
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else {
-      fs.readFile(filename, (err, data) => {
-        if (err) {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          return res.end('404 Not Found');
-          // fs.readFile('./404.html', (anotherError, data) => {
-          //   if (anotherError) {
-          //     console.error(anotherError);
-          //   }
-          //   res.writeHead(404, { 'Content-Type': 'text/html' });
-          //   res.write(data);
-          //   return res.end('AAAA');
-          // });
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    }
-  })
-  .listen(8080);
+  if (filename == './' || filename == './index.html') {
+    fs.readFile('./index.html', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  } else if (filename == './about.html' || filename == './contact-me.html') {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    fs.readFile('./error.html', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Listening on port 8080...');
+  console.log('Please open http://localhost:8080');
+});
